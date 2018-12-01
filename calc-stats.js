acb620c732faafa907e3f6209a965e994d69a0b3
zzptichka <shkvorets@gmail.com>
2018-11-30 23:08:01 -0500

    changed statistics to meters instead of #

--- calc-stats.js
+++ calc-stats.js
@@ -2,6 +2,7 @@ const reader = require('geojson-writer').reader
 const writer = require('geojson-writer').writer
 const fs = require('fs');
 const args = require('minimist')(process.argv.slice(2));
+const ruler = require('cheap-ruler')(45.41, 'meters')
 let inPath;
 
 if (args.help) {
@@ -24,17 +25,24 @@ try {
 
 const Stats = {};
 let totalHighways=0;
+let totalMeters=0;
 const inFile = reader(inPath)
 for(let feature of inFile.features) {
-  for(let key of Object.keys(feature.properties)){
-    if(key=='id') continue;
-    if(key=='highway') totalHighways++;
+  const meters = ruler.lineDistance(feature.geometry.coordinates)
+    for(let key of Object.keys(feature.properties)){
+    if(key=='id' || feature.geometry.type=='Point') continue;
+    if(key=='highway') {
+      totalHighways++;
+      totalMeters+=meters;
+    }
     const tag = `${key}:${feature.properties[key]}`;
     if(typeof(Stats[tag])=='undefined') Stats[tag]=0;
-    Stats[tag]++;
+    //Stats[tag]++;
+    Stats[tag]+=meters;
   }
 }
 Stats['total_highways'] = totalHighways;
+Stats['total_meters'] = totalMeters;
 const statsFeature = {
   "type": "Feature",
   "properties": Stats,
