f496aa9d4b4aa9430eede6817bb4e0619311a023
zzptichka <shkvorets@gmail.com>
2020-03-02 11:07:24 -0500

    desire script into separate file for flexibility

---
+++ build-desire.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+echo "========================================================"
+echo "Starting winter build on `date`"
+echo "========================================================"
+
+NAME=desire
+QUERY_FILE=./osm/$NAME.query
+OSM_FILE=./osm/$NAME.osm
+JSON_FILE=./osm/$NAME.json
+JSON_OLD_FILE=./osm/$NAME-old.json
+
+
+#MAPBOX=mapbox                 #for Mac
+MAPBOX=~/.local/bin/mapbox   #for Linux
+export MAPBOX_ACCESS_TOKEN="sk.eyJ1IjoiYmlrZW90dGF3YSIsImEiOiJjamdqbmR2YmYwYzIyMzNtbmtidDQyeXM0In0.PNr-pb7EPHOcZ2vjikeVFQ"
+OSMTOGEOJSON=/usr/local/bin/osmtogeojson
+#OSMTOGEOJSON=osmtogeojson
+GEOJSONPICK=/usr/local/bin/geojson-pick
+#GEOJSONPICK=geojson-pick
+PICKTAGS="highway surface width id"
+
+cd ~/backend.bikeottawa.ca
+
+echo "Processing and uploading desire lines data ..."
+
+if [ ! -e $QUERY_FILE ]; then
+  echo "Error: Missing desire query file $QUERY_FILE"
+  exit 1
+fi
+
+rm $OSM_FILE
+mv $JSON_FILE $JSON_OLD_FILE
+
+wget -nv -O $OSM_FILE --post-file=$QUERY_FILE "http://overpass-api.de/api/interpreter"
+
+if [ $? -ne 0 ]; then
+  echo "Error: There was a problem running wget."
+  exit 1
+fi
+
+$OSMTOGEOJSON -m $OSM_FILE | $GEOJSONPICK $PICKTAGS > $JSON_FILE
+
+if [ $? -ne 0 ]; then
+  echo "Error: There was a problem running osmtogeojson."
+  exit 1
+fi
+
+if cmp -s $JSON_FILE $JSON_OLD_FILE; then
+    echo 'No changes'
+    exit 0
+fi
+
+$MAPBOX upload bikeottawa.4hnbbuhd $JSON_FILE
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to upload desire lines tileset to Mapbox."
+  exit 1
+fi
